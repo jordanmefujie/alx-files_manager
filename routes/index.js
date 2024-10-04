5fdad1a7155c3eaaa36d53c8fdf5e94184476204
jordanmefujie <mjordanlopo@gmail.com>
2024-10-03 18:57:47 -0700

    APIs

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,19 @@
 
 import express from 'express';
 import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
 
 const router = express.Router();
 
-// Define the routes for the server
+// Existing routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+
+// New authentication routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 export default router;
