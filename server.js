7b2d90ef3df6bd2878ade38952faeb581da2681b
jordanmefujie <mjordanlopo@gmail.com>
2024-10-03 19:19:04 -0700

    Task 3

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
-// server.js
-import express from 'express';
-import startServer from './libs/boot';
-import injectRoutes from './routes';
-import injectMiddlewares from './libs/middlewares';
+// utils/db.js
 
-const server = express();
+import { MongoClient } from 'mongodb';
+import { config } from 'dotenv';
 
-injectMiddlewares(server);
-injectRoutes(server);
-startServer(server);
+config(); // Load environment variables from .env file
 
-export default server;
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.dbName = database;
+
+    this.client.connect().catch((err) => {
+      console.error('MongoDB connection error:', err);
+    });
+  }
+
+  // Check if the connection is alive
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  // Get the number of users in the 'users' collection
+  async nbUsers() {
+    const db = this.client.db(this.dbName);
+    return db.collection('users').countDocuments();
+  }
+
+  // Get the number of files in the 'files' collection
+  async nbFiles() {
+    const db = this.client.db(this.dbName);
+    return db.collection('files').countDocuments();
+  }
+}
+
+// Create and export an instance of DBClient
+const dbClient = new DBClient();
+export default dbClient;
