efb5b1f339f417931774ee0d3203cd6aa3a60f8d
jordanmefujie <mjordanlopo@gmail.com>
2024-10-03 19:28:40 -0700

    New routes added

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import express from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
 import AuthController from '../controllers/AuthController.js';
+import FilesController from '../controllers/FilesController.js';
 
 const router = express.Router();
 
@@ -11,10 +12,11 @@ const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-
-// New authentication routes
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+
+// New file upload route
+router.post('/files', FilesController.postUpload);
 
 export default router;
